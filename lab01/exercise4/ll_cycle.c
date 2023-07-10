35be77d608d44c93c02fd21e38f86c99460c61e0
Wajahat Riaz <wajahatriazmirza@gmail.com>
2023-07-10 11:30:14 +0500

    lab01 exercise4

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -3,4 +3,21 @@
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    if (head == NULL || head->next == NULL) {
+        return 0;  // No cycle if the list is empty or has only one node
+    }
+    
+    node *slow = head;
+    node *fast = head->next;
+    
+    while (fast != NULL && fast->next != NULL) {
+        if (slow == fast) {
+            return 1;  // Cycle detected
+        }
+        
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    
+    return 0;  // No cycle detected
 }
